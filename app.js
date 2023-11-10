2f535ff44402126b09a4de239e26f4484817c765
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-11-10 07:30:40 +0100

    Kodeomformer v1.2

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var powerof = [2**7,2**6,2**5,2**4,2**3,2**2,2**0];
+let ipAdress1 = document.getElementById("ip1").value;
+let ipAdress2 = document.getElementById("ip2");
+let ipAdress3 = document.getElementById("ip3");
+let ipAdress4 = document.getElementById("ip4");
 
+console.log(ipAdress1);
