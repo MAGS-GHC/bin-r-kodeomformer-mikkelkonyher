f19d786f6cc02aa3534ea56d7b37d9c508f6ae2a
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-11-10 11:36:28 +0100

    v1.5

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 let ipAdress1 = document.getElementById("ip1").value;
-let ipAdress2 = document.getElementById("ip2");
-let ipAdress3 = document.getElementById("ip3");
-let ipAdress4 = document.getElementById("ip4");
+let ipAdress2 = document.getElementById("ip2").value;
+let ipAdress3 = document.getElementById("ip3").value;
+let ipAdress4 = document.getElementById("ip4").value;
 let binaryNumber = "";
-
 
 function update() {
    ipAdress1 = document.getElementById("ip1").value
@@ -14,6 +13,15 @@ function update() {
       
    } else {binaryNumber ="0";
 }
+if (ipAdress1 >= 64){
+   ipAdress1 -= 64;
+   binaryNumber += "1"
+} else{binaryNumber="0";
+}
 console.log(binaryNumber);
 binaryNumber ="";
 }
+
+
+//få det i et loop
+
